6c7151c2f5a5ddc50f000697b414010bc51dd500
stegbk@hotmail.com <keithstegbauer@Keiths-MacBook-Pro.local>
2017-01-11 14:49:17 -0800

    Modify build properties so that ant can set the build number

--- PrintCompileTimeNumber/src/printcompiletimenumber/PrintCompileTimeNumber.java
+++ PrintCompileTimeNumber/src/printcompiletimenumber/PrintCompileTimeNumber.java
@@ -5,8 +5,23 @@
  */
 package printcompiletimenumber;
 
+import java.io.File;
+import java.util.Map;
+import java.util.jar.Attributes;
+import java.util.jar.JarFile;
+import java.util.jar.Manifest;
+
 /**
- *
+ * This program is designed to display a number when run after being
+ * compiled with ant.  To set this number:
+ * 
+ * ant -Dbuildnum="21"
+ * 
+ * If the value is -1,  it was probably built without setting that value.
+ * This is used in the testing of Jenkins so that the test can verify the
+ * artifact (jar file) is produced from the latest build and isn't an old 
+ * version.
+ * 
  * @author keithstegbauer
  */
 public class PrintCompileTimeNumber {
@@ -15,7 +30,17 @@ public class PrintCompileTimeNumber {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        try {
+            File currentFile = new File(PrintCompileTimeNumber.class.getProtectionDomain().getCodeSource().getLocation().toURI().getPath());
+            JarFile myJar = new JarFile(currentFile);
+            Manifest manifest = myJar.getManifest();            
+            Map<String,Attributes> manifestContents = manifest.getEntries();
+            Attributes bldnum;
+            bldnum = manifest.getMainAttributes();            
+            System.out.println(bldnum.getValue("Build-Number"));
+        } catch (Exception e) {
+            e.printStackTrace();            
+        }
     }
     
 }
